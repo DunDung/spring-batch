300c07335c68ea7d9b7f0ceeea12dbcbaa422f77
JunSeong <ebseud6135@naver.com>
2020-10-11 15:50:39 +0900

    feat: jpaPagingItemReaderJob 추가

---
+++ spring-batch-study/src/main/java/com/example/springbatch/domain/Pay.java
@@ -0,0 +1,41 @@
+package com.example.springbatch.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Entity
+public class Pay {
+    private static final DateTimeFormatter FORMATTER_BUILDER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long amount;
+    private String txName;
+    private LocalDateTime txDateTime;
+
+    public Pay(Long amount, String txName, LocalDateTime txDateTime) {
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+
+    public Pay(Long id, Long amount, String txName, LocalDateTime txDateTime) {
+        this.id = id;
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+}
