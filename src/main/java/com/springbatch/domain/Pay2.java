3cdedc47d4c55297ee28cd1485345d12dc8e0c5f
JunSeong <ebseud6135@naver.com>
2020-10-11 19:05:04 +0900

    refactor: 프로젝트 파일 변경

---
+++ src/main/java/com/springbatch/domain/Pay2.java
@@ -0,0 +1,41 @@
+package com.springbatch.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Entity
+public class Pay2 {
+    private static final DateTimeFormatter FORMATTER_BUILDER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long amount;
+    private String txName;
+    private LocalDateTime txDateTime;
+
+    public Pay2(Long amount, String txName, LocalDateTime txDateTime) {
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+
+    public Pay2(Long id, Long amount, String txName, LocalDateTime txDateTime) {
+        this.id = id;
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+}
